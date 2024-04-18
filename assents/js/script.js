5a4a888032ea33084fbf383731e93d5e55755a27
EduardoRldan <edu.roldan@duocuc.cl>
2024-04-18 17:38:49 -0400

    Proyecto Semestral

--- assents/js/script.js
+++
@@ -1,7 +0,0 @@
-console.log ('Hola Mundo');
-let nombre = document.getElementById('nombre');
-let btnEnviar = document.getElementById('btnEnviar');
-btnEnviar.addEventListener("click", function(e){
-    e.preventDefault();
-    console.log("Esta ingresanado Bien")
-})
