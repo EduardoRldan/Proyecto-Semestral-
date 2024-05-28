19474ccb63b517b46a2ea5cad19ac710468417ab
EduardoRldan <edu.roldan@duocuc.cl>
2024-05-27 22:12:17 -0400

    Proyecto Semestral

--- assets/js/CreacionUsuario.js
+++ assets/js/CreacionUsuario.js
@@ -1,7 +1,19 @@
-{
-    "name" ; "user-creation-script",
-    "description"; "A  simple script to creater users",
-    "main" ; "CreacionUsuario.js",
-    "script"; {
-        "start "ñ.js";
-    }}
+function register(){
+    const username = document.getElementById('reg-username').value;
+    const email = document.getElementById('reg-email').value;
+    const password = document.getElementById('reg-password').value;
+
+    if (username && email && password){
+        const users = JSON.parse(localStorage,getItem(users)) || [];
+        const usersExists = user.some(user => user.username === username || user.email === email);
+
+        if (usersExists){
+            alert('Usuario o Email ya existentes');
+            return;
+        }
+        user.push({username, email, password});
+        localStorage.setItem('users',JSON.stringify(user));
+        alert('Porfavor clikclea en las casillas');
+    }
+    
+}
