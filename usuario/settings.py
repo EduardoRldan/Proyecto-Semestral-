be5e211c1e535b4a448d3bc64de98b741a497aa7
EduardoRldan <edu.roldan@duocuc.cl>
2024-07-05 00:36:22 -0400

    Proyecto Semestral

--- usuario/settings.py
+++ usuario/settings.py
@@ -1,14 +1,14 @@
 # settings.py
 
 INSTALLED_APPS = [
-    ...
-    'usuarios',  # Asegúrate de que tu aplicación esté registrada aquí
-    ...
+    
+    'usuarios', 
+    
 ]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-LOGIN_REDIRECT_URL = 'home'  # Redirige al usuario a la página de inicio después del inicio de sesión
-LOGOUT_REDIRECT_URL = 'login'  # Redirige al usuario a la página de inicio de sesión después de cerrar sesión
+LOGIN_REDIRECT_URL = 'home'  
+LOGOUT_REDIRECT_URL = 'login' 
