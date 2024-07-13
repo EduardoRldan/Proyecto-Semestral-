b255fa61b84beff19801636c0be27a7315ba2a26
EduardoRldan <edu.roldan@duocuc.cl>
2024-07-12 20:01:30 -0400

    Proyecto Semestral

--- tienda/models.py
+++ tienda/models.py
@@ -37,8 +37,36 @@ def add_initial_Videjuego(apps, schema_editor):
         precio = 60.99,
         fecha_lanzamiento = '2006-11-25',
         editor = 'Activision',
-        imagen = 'Static/img/cod.jpg',
+        imagen = 'Static/img/cod2.jpg',
         )
     
 
-    Videojuego.objects.created()
+    Videojuego.objects.created(
+        nombre = 'Assasin\'s Creed',
+        descripcion = 'Un juego de accion y aventuras',
+        precio = 49.99, 
+        fecha_lanzamiento = '2007/11/16',
+        editor = 'Ubisoft',
+        imagen = 'Static/img/assasa1.jpg',
+
+    )
+
+    Videojuego.objects.create(
+        nombre = 'Need for Speed: Most Wanted',
+        descripcion = 'Un juego de Carreras',
+        precio = 39.99,
+        fecha_lanzamiento = '2005/11/11',
+        desarrollador = 'Ghost Games',
+        editor = 'Electronic Arts',
+        imagen = 'Static/img/needforspped.jpg'
+    )
+
+class Migration(migrations.Migration):
+
+    dependencies =[
+        ('videojuegos', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_initial_Videjuego),
+    ]
